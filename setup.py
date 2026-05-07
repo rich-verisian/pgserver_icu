5aa4e9700b1c2d0203a51a4401b0755ce1a7a48c
Richard Stone <richard@verisian.com>
2026-05-07 18:13:28 +0100

    setup path update

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     setup_requires=["cffi"],
     # dummy but needed for the binaries to work
-    cffi_modules=["src/pgserver/_build.py:ffibuilder"], 
+    cffi_modules=["src/pgserver-icu/_build.py:ffibuilder"], 
 )
